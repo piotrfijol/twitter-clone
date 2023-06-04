5a7b510ee90e32bcf0eae35003c41a34ffde4f7e
Piotr Fijol <piotr.fijol1@gmail.com>
2023-06-04 14:10:18 +0200

    Add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import './App.css'
+import { MobileNavbar } from './components/MobileNavbar'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 function App() {
   
   return (
     <>
+      <Router>
+        <MobileNavbar />
+      </Router>
     </>
   )
 }
